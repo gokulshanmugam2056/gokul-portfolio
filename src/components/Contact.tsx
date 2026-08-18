3eff0b6d87fecb0d2d96f1bfb86fab6f8bf3a119
Gokul Shanmugam <gokulshanmugam2056@gmail.com>
2026-08-18 11:42:00 +0530

    update contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -132,9 +132,7 @@ export const Contact = () => {
                 <div>
                   <p className="font-medium">LinkedIn</p>
 
-                  <p className="text-sm text-muted-foreground">
-                    gokulshanmugam2056
-                  </p>
+                  
                 </div>
 
               </Card>
